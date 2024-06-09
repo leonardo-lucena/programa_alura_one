cdad54c60d9d4b69bfc55bd0e3cfa3321f110ea0
Leonardo Lucena <lucena.lbd@gmail.com>
2024-06-09 01:00:41 -0300

    feat: Otiiza o código substituindo o while por for

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,18 +1,15 @@
-function tocaSom(idAudio){
+function tocaSom(idAudio) {
     document.querySelector(idAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
 
-while(contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
-    tecla.onclick = function(){
+    tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
-    contador++;
 }
