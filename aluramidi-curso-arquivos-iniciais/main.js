8fafa8f1488da36ad4d9b8dfef524cad88246d4e
Leonardo Lucena <lucena.lbd@gmail.com>
2024-06-09 00:58:58 -0300

    feat: Adiciona função generica para tocar cada tecla individualmente

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,18 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idAudio){
+    document.querySelector(idAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    contador++;
+}
