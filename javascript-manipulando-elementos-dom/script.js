6c322586b785cdf72e093de8d5dcd27a4b490179
Leonardo Bandeira <lucena.lbd@gmail.com>
2024-07-09 15:51:30 -0300

    feat: Ajusta o texto para o banner

--- javascript-manipulando-elementos-dom/script.js
+++ javascript-manipulando-elementos-dom/script.js
@@ -3,7 +3,7 @@ const focoBt = document.querySelector(".app__card-button--foco");
 const curtoBt = document.querySelector(".app__card-button--curto");
 const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
-
+const titulo = document.querySelector(".app__title");
 focoBt.addEventListener("click", () => {
   alterarContexto("foco");
 });
@@ -19,4 +19,19 @@ longoBt.addEventListener("click", () => {
 function alterarContexto(contexto) {
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `./imagens/${contexto}.png`);
+
+  if (contexto == "foco") {
+    titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+  }
+
+  if (contexto == "descanso-curto") {
+    titulo.innerHTML = `Texto 2,<br>
+                <strong class="app__title-strong">texto 2.</strong>`;
+  }
+
+  if (contexto == "descanso-longo") {
+    titulo.innerHTML = `Texto 3,<br>
+                <strong class="app__title-strong">Texto 3.</strong>`;
+  }
 }
